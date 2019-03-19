f1654f4a6e540d206c3723be7dbf2e3e1c2207b3
Administrator <Administrator@DESKTOP-N7UPK4S>
2019-03-19 17:45:35 +0800

    test

---
+++ Web/Areas/test3/test3AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.test3
+{
+    public class test3AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "test3";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "test3_default",
+                "test3/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
